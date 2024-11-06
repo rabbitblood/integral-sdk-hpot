fb8c8b434fc683f655597de2f1bb9bd80f7d4a85
Hongming Wang <70198795+rabbitblood@users.noreply.github.com>
2024-11-06 01:43:06 -0800

    Update addresses.ts

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -2,8 +2,10 @@ import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
     [ChainId.Holesky]: '0x4777378A908A90862AdDedabF9388958Cbd020f1',
+    [ChainId.BerachainTestnet]: '0x805488DaA81c1b9e7C5cE3f1DCeA28F21448EC6A',
 }
 
 export const POOL_INIT_CODE_HASH = {
     [ChainId.Holesky]: '0x4b9e4a8044ce5695e06fce9421a63b6f5c3db8a561eebb30ea4c775469e36eaf',
+    [ChainId.BerachainTestnet]: '0x3093a65c28d1e6def42997fdea76d033dfd42b432c107e19412cf91a1eac0f91',
 }
